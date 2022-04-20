9d07033de7d5b3daf5d702059f937ed12566b0bb
Anıl Başaran <35750219+AnilBASARAN@users.noreply.github.com>
2022-04-20 11:05:59 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function solution(str){
+    return str.split('').reverse().join('')
+    
+  }
+
+  /*
+  8 kyu Reversed Strings
+
+  Complete the solution so that it reverses the string passed into it.
+
+'world'  =>  'dlrow'
+'word'   =>  'drow'
